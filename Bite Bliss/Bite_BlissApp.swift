5cbbee6ecf7edf03ac15c20f2eafbfbbcf137fbc
Harish <115090388+Rish-Bot@users.noreply.github.com>
2023-10-03 11:41:19 +0530

    Initial Commit

---
+++ Bite Bliss/Bite_BlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bite_BlissApp.swift
+//  Bite Bliss
+//
+//  Created by Hari on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Bite_BlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
